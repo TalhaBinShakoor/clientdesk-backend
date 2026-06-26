d3c4169dcb4255b719151994357fbcb73886e1ee
TalhaBinShakoor <talhabinshakoor100@gmail.com>
2026-06-26 22:23:56 +0200

    Initial Spring Boot backend scaffold

---
+++ src/main/java/com/clientdesk/ClientdeskBackendApplication.java
@@ -0,0 +1,13 @@
+package com.clientdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientdeskBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientdeskBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clientdesk/ClientdeskBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clientdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientdeskBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
